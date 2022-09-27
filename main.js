7db1558340eb9d2c417dd0bb8d975b707356e796
Wojtek Miedziarek <wojciech.miedziarek@gmail.com>
2022-09-27 13:25:15 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+console.log('Próba tekstu, próba tekstu 123, 123');
+
+const firstName = "Wojtek";
+const age = 32
+const cofee = "kawa";
+
+console.log(firstName);
+console.log(age);
+console.log(cofee);
+
+console.log(`Cześć mam na imię ${firstName} a mój wiek to ${age} lata. Lubię pić ${cofee}. Uczę się programować, żeby mimo mojego wieku, który wynosi ${age} lata, móc zmienić zawód i powiedzieć sobie ${firstName} udało Ci się.`);
